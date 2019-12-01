64455bfad4040569ea87633456dff356d999a40d
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2019-11-30 20:16:21 -0800

    Add files via upload

---
+++ Project1_AIassistant/AI_Assistance.py
@@ -0,0 +1,122 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+from random import randint
+
+emaildict = {'jahin': 'jahincse14@gmail.com','munim':'munimhossain411@gmail.com','hulo':'ik21.cse14@gmail.com'}
+
+engine =pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voice)
+engine.setProperty('voice',voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour <= 11:
+        speak('Good morning sir !')
+    elif hour >= 12 and hour <= 15:
+        speak('Good afternoon sir !')
+    elif hour>=16 and hour<=19:
+        speak('Good evening sir !')
+    else:
+        speak('Good night sir !')
+    speak('I am sophia, your assistant. Please tell me how may I help you sir!')
+
+
+def  takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print('Recognizing..')
+        query = r.recognize_google(audio, language='en-in')
+        #print(f'User said {query}\n')
+    except Exception as e:
+        print('Say that again please...')
+        return 'None'
+    return query
+
+def recipient(s):
+    index = s.index('to')
+    recipient = s[(index+3):]
+    return recipient
+
+def sendEmail(to,content):
+
+
+
+if __name__ == "__main__":
+    wishme()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            print('Searceing wikipedia')
+            speak('Searceing wikipedia')
+            query = query.replace('wikipedia','')
+            results = wikipedia.summary(query,sentences=2)
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open('youtube.com')
+        elif 'open codeforce' in query:
+            webbrowser.open('codeforces.com')
+        elif 'open hackerrank' in query:
+            webbrowser.open('hackerrank.com')
+        elif 'open google' in query:
+            webbrowser.open('google.com')
+        elif 'open stackoverflow' in query:
+            webbrowser.open('stackoverflow.com')
+        elif 'play music' in query:
+            music_dir = 'G:\\Adio\\Music'
+            songs = os.listdir(music_dir)
+            #print(songs)
+            index = randint(0,len(songs)-1)
+            songName = songs[index].replace('_',' ')
+            songName = songName.replace('mp3','')
+            songName = songName.replace('.',' ')
+            speak(f"Playing {songName} for you sir!")
+            os.startfile(os.path.join(music_dir,songs[index]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"sir, The time is {strTime}")
+
+        elif 'open chrome' in query:
+            path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            os.startfile(path)
+
+        #elif 'open code' in query:
+         #   path = "C:\\Program Files (x86)\\CodeBlocks\\codeblocks.exe"
+          #  os.startfile(path)
+
+        elif 'open powerpoint' in query:
+            path = "C:\\Program Files\\LibreOffice\\program\\soffice.exe"
+            os.startfile(path)
+
+        elif 'send email' in query:
+            try:
+                speak('What shoult i say?')
+                content = takeCommand()
+                to =  emaildict[recipient(query)]
+                sendEmail(to,content)
+                speak('Email has been send')
+            except Exception as e:
+                speak(f'I am sorry for {recipient(query)} I am not able to send him mail this time!')
+
+
+
+
+
+
+
+
---
+++ Project1_AIassistant/tst.py
@@ -0,0 +1,3 @@
+s='send email to hulo'
+index=s.index('to')
+print(s[(index+3):])
